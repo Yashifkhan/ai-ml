ac7bd37d8dfd82969179c7cedda9d87478791699
yashif khan <yashifyashifkhan@gmail.com>
2026-03-16 00:13:55 +0530

    learn loop or range function

---
+++ fundamentels/index.py
@@ -0,0 +1,19 @@
+# print the multplication table of any number 
+# n=int(input("Enter the number : "))
+# print(n)
+# def print_table(n):
+#     for i in range(1,11):
+#         print(n,"*" , i, "=",n*i)
+# print_table(n)
+
+
+
+# count the frequence of number in str
+str="yashifaa"
+count=0
+for i in str:
+    if i == "a":
+        count +=1
+
+print(count)
+
