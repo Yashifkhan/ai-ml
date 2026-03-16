a313c50676a9ecc4f0d4e84d7cdd8ef6660e95c6
yashif khan <yashifyashifkhan@gmail.com>
2026-03-16 21:58:21 +0530

    learn data type and function or lambda fun day 4 complete

--- fundamentels/index.py
+++ fundamentels/index.py
@@ -9,11 +9,57 @@
 
 
 # count the frequence of number in str
-str="yashifaa"
-count=0
-for i in str:
-    if i == "a":
-        count +=1
+# str="yashifaa"
+# count=0
+# for i in str:
+#     if i == "a":
+#         count +=1
 
-print(count)
+# print(count)
+
+# create a function thats take 3 paramters and return the sum of avg 
+# n1=int(input("Enter 1st Number :"))
+# n2=int(input("Enter 2nd Number :"))
+# n3=int(input("Enter 3rd Number :"))
+
+# def sum_avg(n1,n2,n3):
+#     sum=(n1+n2+n3)
+#     result=round(sum,2)
+#     return result/3
+# print(sum_avg(n1,n2,n3))
+
+# print the fectorial of a number 
+# n=int(input("Enter the number : "))
+# def fect_num(n):
+#     fect=1
+#     for i in range(1,n+1):
+#         fect= fect*i 
+#     return fect
+# print(fect_num(n))
+
+
+# . Write a program that takes as input. Using conditional statements, 
+# calculate the final tax rate based on these rules:
+
+# If salary < 30,000 → 5% 
+# • If salary is 30,000–70,000 → 15% 
+# • If salary > 70,000 → 25% 
+
+
+# salary=int(input("Enter Your Salary: "))
+# def tax_rate(salary):
+#     if salary <= 30000:
+#         print("You pay 5 % tax : ")
+#         print("Tax is ",salary*(5/100))
+#         print("Your salary after tax is : ",salary+(salary*(5/100)))
+#     elif (salary >30000 and salary <= 70000):
+#         print("You pay 15 % tax : ")
+#         print("Tax is ",salary*(15/100))
+#         print("Your salary after tax is : ",salary+(salary*(15/100)))
+#     else :
+#         print("You pay 25 % tax : ")
+#         print("Tax is ",salary*(25/100))
+#         print("Your salary after tax is : ",salary+(salary*(5/100)))
+    
+# tax_rate(salary)
 
