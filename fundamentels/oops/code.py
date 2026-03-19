1acaa5eb033dd5b39b074b4084cb73d4494a5d84
yashif khan <yashifyashifkhan@gmail.com>
2026-03-19 23:25:06 +0530

    learn oops in python some prectice question

--- fundamentels/oops/code.py
+++ fundamentels/oops/code.py
@@ -1,1 +1,14 @@
-print("start larning oops")
+# print("start larning oops")
+class students:
+    def __init__(self,name,age,cgpa):
+        self.name =name
+        self.age = age
+        self.cgpa = cgpa
+    
+    def get_cgpa(self):
+        return self.cgpa
+    
+std1=students("yashif",22,9.5)
+std2=students("hello",20,8.5)
+print("cgpa of yashif : ",std1.get_cgpa())
+print("cgpa of hello : ",std2.get_cgpa())
