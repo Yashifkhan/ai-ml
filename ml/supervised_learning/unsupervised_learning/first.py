98d9c34e82a82d5c5b9b71d13596243d5d94ad40
yashif khan <yashifyashifkhan@gmail.com>
2026-04-10 23:51:44 +0530

    learn application of ml and life cycle of o ml model

--- ml/supervised_learning/unsupervised_learning/first.py
+++ ml/supervised_learning/unsupervised_learning/first.py
@@ -1,2 +1,3 @@
 print("how to train a modal on online and batch traning")
 print("learn instence based learning and model based learning")
+print("learn application of ml and life cycle of o ml model")
