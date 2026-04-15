4c6a117de30fe0f5cf019311e691d6d0bd69f52e
yashif khan <yashifyashifkhan@gmail.com>
2026-04-15 23:17:08 +0530

    learn about the feature engineering

--- ml/supervised_learning/underfit_ovefit.py
+++ ml/supervised_learning/underfit_ovefit.py
@@ -1,2 +1,3 @@
 print("tranining data and test data is low perform model the it is undefit")
 print("traning data is hihg >> to  test data then modcel is overfit ")
+print("traning data is hihg >> to  test data then modcel is overfit ")
