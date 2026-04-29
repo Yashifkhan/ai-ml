90c0dd6f6ae08cf56a6eef56f6e760e53e63ba26
yashif khan <yashifyashifkhan@gmail.com>
2026-04-29 23:50:46 +0530

    learn linear regression from scratch

--- linear_regression/simple_linear_reg.py
+++ linear_regression/simple_linear_reg.py
@@ -1,43 +1,82 @@
-print("learn simple linear regression ")
+# print("learn simple linear regression ")
 
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-# load the data 
-df=pd.read_csv("std_placement.csv")
+# import pandas as pd
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+# import pandas as pd
+# from sklearn.linear_model import LinearRegression
+# from sklearn.model_selection import train_test_split
+# # load the data 
+# df=pd.read_csv("std_placement.csv")
 
-# plot the data on graph 
-sns.scatterplot(
-    x=df["Hours"],
-    y=df["Scores"]
-)
-plt.title("Student Study hours scores ")
+# # plot the data on graph 
+# sns.scatterplot(
+#     x=df["Hours"],
+#     y=df["Scores"]
+# )
+# plt.title("Student Study hours scores ")
 
 
-# extract the features 
-X=df[["Hours"]]
-y=df["Scores"]
+# # extract the features 
+# X=df[["Hours"]]
+# y=df["Scores"]
 
-# take row and column paramets 
-# X=df.iloc[0:5]
+# # take row and column paramets 
+# # X=df.iloc[0:5]
 
-# spllit the data 
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=.2,random_state=42)
+# # spllit the data 
+# X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=.2,random_state=42)
 
-# define the model & give the data to learn
-model=LinearRegression()
-model.fit(X_train,y_train)
+# # define the model & give the data to learn
+# model=LinearRegression()
+# model.fit(X_train,y_train)
 
-# Predict
-y_pred = model.predict(X_test)
 
-sample = pd.DataFrame([[1]], columns=X_train.columns)
-prediction = model.predict(sample)
+# # Predict
+# y_pred = model.predict(X_test)
 
-print("predict score is : ",prediction)
-plt.show()
+# sample = pd.DataFrame([[1]], columns=X_train.columns)
+# prediction = model.predict(sample)
+
+
+# print("predict score is : ",prediction)
+# plt.show()
 
 # print(df.head())
+
+
+
+
+
+# part 2  
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, r2_score
+
+# Sample dataset
+data = {
+    "Hours": [1,2,3,4,5],
+    "Score": [40,50,60,70,80]
+}
+
+df = pd.DataFrame(data)
+
+X = df[["Hours"]]
+y = df["Score"]
+
+# Split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Train
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Predict
+pred = model.predict(X_test)
+
+
+# Evaluate
+print("MAE:", mean_absolute_error(y_test, pred))
+print("pred",pred)
+# print("R2:", r2_score(y_test, pred))
