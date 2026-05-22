7b96d6678381d9c5ab591b0209acd0531e621614
yashif khan <yashifyashifkhan@gmail.com>
2026-05-22 23:53:19 +0530

    start 30 day of challange learn complete supervised ml

---
+++ 30_days_supervised_ml/day_one.py
@@ -0,0 +1,2 @@
+# print("start learn the what is ml and supervised ml ")
+print("learn the basics of Regression and Classification")
--- ml/supervised_learning/unsupervised_learning/first.py
+++
@@ -1,5 +0,0 @@
-print("how to train a modal on online and batch traning")
-
-print("learn instence based learning and model based learning")
-print("learn application of ml and life cycle of o ml model")
-print("learn about tensor and vector")
