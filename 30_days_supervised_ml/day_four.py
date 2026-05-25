6fe0a296ca7774159aa0daec1322f068e7959f72
yashif khan <yashifyashifkhan@gmail.com>
2026-05-25 22:56:21 +0530

    day 4 learn simple linear regression

---
+++ 30_days_supervised_ml/day_four.py
@@ -0,0 +1,19 @@
+# import pandas as pd 
+# from sklearn.linear_model import LinearRegression
+
+# data = pd.DataFrame({
+#     "hours": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+#     "marks": [35, 40, 50, 55, 60, 65, 70, 75, 85, 95]
+# })
+
+
+# X=data[["hours"]]
+# y=data["marks"]
+# model=LinearRegression()
+# model.fit(X,y)
+
+# predect_value=model.predict([[12]])
+# print("Model predict value is : ",predect_value)
+
+
+# prectice 2 
