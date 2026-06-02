6bcad3fca3fe21ca9288a4894f7240b2cc20b54d
yashif khan <yashifyashifkhan@gmail.com>
2026-06-02 23:50:45 +0530

    day 8 start learning classification

---
+++ 30_days_supervised_ml/day_8/index.py
@@ -0,0 +1,14 @@
+# main step to use every project in classificaton problem 
+# step 1 : remove the outlires , if not remove then line is wrong plot the mid is chnage so we do not predict the perfect category 
+# influnce the stratight  line ,mean chnage the strt line
+# step 2 result can be positive or negitive 
+
+# so we prevent to this mistake we use the squash the line s type 
+# squash line also called logistic funtion s type courv
+
+# if we create the squash line (s tyoe cruv ) then we use sigmoid active  funtion
+
+# sigmoid active  funtion formula 
+ 
+ 
+#  best fit line create with the help of cost function 
