e2e3561c37761c49ccd30b88474c64709ef748f8
yashif khan <yashifyashifkhan@gmail.com>
2026-06-05 21:03:02 +0530

    learn the knn algorithm

---
+++ 30_days_supervised_ml/day_11/index.py
@@ -0,0 +1,31 @@
+# print("i want to learn the knn")
+# 
+# without sk learn algorithm 
+# data = [
+#     (100, "apple"),
+#     (110, "apple"),
+#     (120, "apple"),
+#     (150, "orange"),
+#     (160, "orange"),
+#     (170, "orange")
+# ]
+
+# distance=[]
+# new_weight=130
+# k=3
+# for weight ,label in data:
+#     dist=abs(weight - new_weight)
+#     distance.append((dist,label))
+
+# distance.sort()
+# k_nearest=distance[:3]
+
+# votes={}
+# for _, label in k_nearest:
+#     votes[label]=votes.get(label,0)+1
+    
+# prediction = max(votes, key=votes.get)
+# print("Prediction:", prediction)
+
+
+# with algorith build features use 
