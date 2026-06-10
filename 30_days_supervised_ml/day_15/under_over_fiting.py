9f35a6dae90a883cc8bf8e7f259fc832126534d0
yashif khan <yashifyashifkhan@gmail.com>
2026-06-10 23:27:44 +0530

    learn the underfiting and overfiting

---
+++ 30_days_supervised_ml/day_15/under_over_fiting.py
@@ -0,0 +1,21 @@
+# Bias = Error due to too simple assumptions 
+# Variance = Error due to too much sensitivity to data
+
+# In Machine Learning, your goal is:
+# Build a model that learns patterns, not just memorizes data.
+
+# Underfitting (High Bias)
+# Model is too simple
+# Cannot capture real patterns
+
+# Overfitting (High Variance)
+
+# Model is too complex
+# Memorizes training data
+
+
+# Student Example
+# Type	Behavior
+# Underfitting	Student didn’t study → fails everywhere
+# Good Fit	Student understands concepts
+# Overfitting	Student memorizes answers → fails in new questions
