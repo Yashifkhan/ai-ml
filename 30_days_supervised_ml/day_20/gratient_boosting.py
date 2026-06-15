071a0f75d47ad182c6634af133706a6b8e56220e
yashif khan <yashifyashifkhan@gmail.com>
2026-06-15 23:17:09 +0530

    learn gradient boosting

---
+++ 30_days_supervised_ml/day_20/gratient_boosting.py
@@ -0,0 +1,13 @@
+
+# Boosting is a technique where models are built sequentially.
+
+# First model learns from data
+# Second model learns from mistakes of first model
+# Third model learns from mistakes of first + second
+# This continues…
+
+# Final prediction = sum of all weak models
+
+# Instead of one strong model, we combine many weak learners (small decision trees).
+
+# So accuracy keeps improving step by step.
