0078936f29c646ba63178fd8acee6cf5ee03d2bc
yashif khan <yashifyashifkhan@gmail.com>
2026-06-16 23:06:07 +0530

    learn the gradient boosting and difference b/w the another boosting techinque

--- 30_days_supervised_ml/day_20/gratient_boosting.py
+++ 30_days_supervised_ml/day_20/gratient_boosting.py
@@ -11,3 +11,25 @@
 # Instead of one strong model, we combine many weak learners (small decision trees).
 
 # So accuracy keeps improving step by step.
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.datasets import load_breast_cancer
+
+data = load_breast_cancer()
+X = data.data
+y = data.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = GradientBoostingClassifier(
+    n_estimators=100,   # number of trees
+    learning_rate=0.1,  # step size
+    max_depth=3
+)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
