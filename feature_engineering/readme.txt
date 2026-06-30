866a012c7a33d99e448f33e681c6987f0296145e
yashif khan <yashifyashifkhan@gmail.com>
2026-06-30 23:51:27 +0530

    apply the feature engineering on emp salray pre data

---
+++ feature_engineering/readme.txt
@@ -0,0 +1,7 @@
+chat link 
+https://chatgpt.com/c/6a43de3d-d528-83ee-a824-f1ed00e03b6f
+
+every proje t use this step to feature engineering 
+Before feature engineering, divide features into types:
+Numerical Features,Categorical Features,Multi-Value Features
+
--- projects/emp_salary_predicter/app/train_model.py
+++ projects/emp_salary_predicter/app/train_model.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 df=pd.read_csv("../data/survey_results_public.csv")
 
-
+# data understanding and 
 # print the all uniqe skills user select in which 
 # skills_column = df["LanguageHaveWorkedWith"]
 # skills_column = skills_column.dropna()
@@ -75,18 +75,102 @@ df=pd.read_csv("../data/survey_results_public.csv")
 # unique_db = set(flat_skills)
 # print("Total unique db  skills:", len(unique_db))
 # print(unique_db)
-
-
-# db_skills = df["Country"]
-# db_column = db_skills.dropna()
-# all_db = db_column.str.split(";")
-# flat_skills = [skill for sublist in all_db for skill in sublist]
-# unique_db = set(flat_skills)
-# print("Total unique db  skills:", len(unique_db))
-# print(unique_db)
-
-
 # print("data filter proper for the ui filters ")
 
-# print(df.columns)
-# print(df.head())
+
+# step 2 feature engineering 
+
+# get the requred feature 
+required_features=[
+         "YearsCodePro",
+         "WorkExp",
+         "EdLevel",
+         "DevType",
+         "OrgSize",
+         "Industry",
+         "RemoteWork",
+         "Country",
+         "LanguageHaveWorkedWith",
+         "PlatformHaveWorkedWith",
+         "DatabaseHaveWorkedWith",
+         "ToolsTechHaveWorkedWith",
+         "ConvertedCompYearly",
+
+]
+
+
+# divide the feature based on there type 
+def clean_experience(x):
+    if pd.isna(x):
+        return None
+    x = str(x)
+    if "Less than 1 year" in x:
+        return 0.5
+    elif "More than 50 years" in x:
+        return 50
+    else:
+        try:
+            return float(x)
+        except:
+            return None
+
+def is_multivalue_column(series):
+    sample = series.dropna().astype(str).head(50)
+    
+    # Count how many rows contain ';'
+    count = sample.str.contains(';').sum()
+    
+    # If many rows have ';' → multi-value
+    return count > 5
+
+df['YearsCodePro'] = df['YearsCodePro'].apply(clean_experience)
+df['WorkExp'] = pd.to_numeric(df['WorkExp'], errors='coerce')
+def devide_data_bytype(df,required_features, target_column):
+    cat_features = []
+    num_features = []
+    multi_value_features = []
+
+    for col in df[required_features]:
+        if col == target_column:
+            continue
+
+        if pd.api.types.is_numeric_dtype(df[col]):
+            num_features.append(col)
+
+        elif pd.api.types.is_object_dtype(df[col]):
+            if is_multivalue_column(df[col]):
+                multi_value_features.append(col)
+            else:
+                cat_features.append(col)
+
+    return {
+        "numerical": num_features,
+        "categorical": cat_features,
+        "multi_value": multi_value_features,
+        "target": target_column
+    }
+result=devide_data_bytype(df,required_features,"ConvertedCompYearly")
+# print(devided_data_result)
+# Numerical
+df[result['numerical']] = df[result['numerical']].fillna(
+    df[result['numerical']].median()
+)
+
+# Categorical
+for col in result['categorical']:
+    df[col] = df[col].fillna("Unknown")
+
+# Multi-value
+for col in result['multi_value']:
+    df[col] = df[col].fillna("")
+
+# Target
+df = df[df['ConvertedCompYearly'].notna()]
+df.drop_duplicates()
+
+# print(required_features)
+print(df[required_features])
+print(df[required_features].isnull().sum())
+# print(df[requred_features])
+# print(required_features)
+# print(len(required_features))
